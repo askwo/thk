0b784991a7a4e8ce1953a657a865bcd77edcdbc1
askwo <13728026@qq.com>
2016-02-29 13:41:47 +0800

    Added files via upload

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+if(empty($_FILES['photo']['name']))
+	{
+		$newpicname="";
+	}
+	else
+	{
+	
+	//图片上传
+    $date=date('Ymdhis');//得到当前时间,如;20070705163148
+    $name=explode('.',$_FILES['photo']['name']);//将文件名以'.'分割得到后缀名,得到一个数组
+    $newpicname=$date.'.'.$name[1];//得到一个新的文件为'20070705163148.jpg',即新的路径
+    $path='uploadfile/'.$newpicname;
+    $ups=move_uploaded_file($_FILES['photo']['tmp_name'],$path);
+   }
+   
+
+    //function printVars($a,$b = ''){
+	//	printf("%d%d\n",$a,$b);
+	//}
+	//printVars(1,2,3);
+	//printf("-------------\n");
+	//printVars(1);
+	//$conn=@mysql_pconnect("113.10.144.2","bugcms_f","821182") or die("数据库连接失败：".mysql_error());
+	?>
